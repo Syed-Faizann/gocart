7bbbe96d9c035e2bdfb7484a6ef5b864132d6bb7
Syed-Faizann <syedfaizanali2122@gmail.com>
2025-11-16 02:10:48 +0500

    api

---
+++ app/api/store/product/route.js
@@ -0,0 +1,106 @@
+import { getAuth } from "@clerk/nextjs/server";
+import authSeller from "@/middlewares/authSeller";
+import { NextResponse } from "next/server";
+import imagekit from "@/config/imageKit";
+import prisma from "@/lib/prisma";
+import { err } from "inngest/types";
+
+// Add a new product
+
+export async function POST(request) {
+  try {
+    const { userId } = getAuth(request);
+    const storeId = await authSeller(userId);
+
+    if (!storeId) {
+      return NextResponse.json({ error: "not authorized" }, { status: 401 });
+    }
+
+    // Get the data from the form
+    const formData = await request.formData();
+    const name = formData.get("name");
+    const description = formData.get("description");
+    const mrp = Number(formData.get("mrp"));
+    const price = Number(formData.get("price"));
+    const category = formData.get("category");
+    const images = formData.get("images");
+
+    if (
+      !name ||
+      !description ||
+      !mrp ||
+      !price ||
+      !category ||
+      !images ||
+      images.length < 1
+    ) {
+      return NextResponse.json(
+        { error: "missing product details" },
+        { status: 400 }
+      );
+    }
+
+    //upload images to imagekit
+    const imageUrl = await Promise.all(
+      images.map(async (image) => {
+        const buffer = Buffer.from(await image.arrayBuffer());
+        const response = await imagekit.upload({
+          file: buffer,
+          fileName: image.name,
+          folder: "products",
+        });
+        const url = imagekit.url({
+          path: response.filePath,
+          transformation: [
+            { quality: "auto" },
+            { format: "webp" },
+            { width: "1024" },
+          ],
+        });
+        return url;
+      })
+    );
+
+    await prisma.product.create({
+      data: {
+        name,
+        description,
+        mrp,
+        price,
+        category,
+        images: imageUrl,
+        storeId,
+      },
+    });
+
+    return NextResponse.json({ message: "Product added successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
+
+// Get all products for a seller
+
+export async function GET(request) {
+  const { userId } = getAuth(request);
+  const storeId = await authSeller(userId);
+  try {
+    if (!storeId) {
+      return NextResponse.json({ error: "not authorized" }, { status: 401 });
+    }
+    const products = await prisma.product.findMany({
+      where: { storeId },
+    });
+    return NextResponse.json({ products });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
