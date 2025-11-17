b10079d87578781fa2901f0eff87acd9f80012ff
Syed-Faizann <syedfaizanali2122@gmail.com>
2025-11-18 03:23:33 +0500

    admin

---
+++ middlewares/authAdmin.js
@@ -0,0 +1,39 @@
+// const { clerkClient } = require("@clerk/nextjs/server")
+
+// const authAdmin =  async (userId) =>{
+//     try{
+//         if(!userId) return false
+//         const client = await clerkClient()
+//         const user = await client.users.getUser(userId)
+
+//         return process.env.ADMIN_EMAIL.split(',').includes(user.emailAddresses[0].emailAddress)
+//     }catch(error){
+//         console.error(error)
+//         return false
+//     }
+// }
+
+// export default authAdmin()
+import { clerkClient } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
+
+const authAdmin = async (userId) => {
+  try {
+    if (!userId) return false;
+
+    // Get user from Clerk
+    const user = await clerkClient.users.getUser(userId);
+
+    const userEmail = user.emailAddresses[0].emailAddress;
+
+    // Check if the email exists in admin email list
+    const adminEmails = process.env.ADMIN_EMAIL.split(",");
+
+    return adminEmails.includes(userEmail);
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
+export default authAdmin;
